1dae3a7cc77996a3b8ee3ff87657da5f724f9f67
Jovica Jovićević <jovica.jovicevic@googlemail.com>
2021-07-08 10:01:21 +0200

    Added boundaries to camera movement, changed zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,15 +9,17 @@ public class CameraController : MonoBehaviour
 	[SerializeField]
 	private float cameraSpeed = 100f;
 
-	private Vector3 target;
+	private Vector3 target = new Vector3(0, 0, 0);
 	private float distance = 10f;
+	private float magnitudeY = 89f;
 
 	private Transform cameraTrans;
+	private Camera cam;
 
 	void Start()
 	{
 		cameraTrans = gameObject.GetComponent<Transform>();
-		target = new Vector3(0, 0, 0);
+		cam = gameObject.GetComponent<Camera>();
 	}
 
 	void Update()
@@ -25,12 +27,24 @@ public class CameraController : MonoBehaviour
 		float rotY = Input.GetAxis("Horizontal") * cameraSpeed * Time.deltaTime;
 		float rotX = Input.GetAxis("Vertical") * cameraSpeed * Time.deltaTime;
 
+		float currentY = cameraTrans.localEulerAngles.x + (cameraTrans.localEulerAngles.x >= 270f ? -360f : 0f);
+
+		if (currentY + rotX > magnitudeY)
+		{
+			rotX = magnitudeY - currentY;
+		}
+		else if (currentY + rotX < -magnitudeY)
+		{
+			rotX = -magnitudeY - currentY;
+		}
+
 		cameraTrans.transform.position = target;
 
 		cameraTrans.transform.Rotate(new Vector3 (1, 0, 0), rotX);
 		cameraTrans.transform.Rotate(new Vector3 (0, 1, 0), -rotY, Space.World);
 
-		distance -= Input.mouseScrollDelta.y * cameraZoomSpeed;
 		cameraTrans.transform.Translate(new Vector3(0, 0, -distance));
+
+		cam.fieldOfView -= Input.mouseScrollDelta.y * cameraZoomSpeed;
 	}
 }
